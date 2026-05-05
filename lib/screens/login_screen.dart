d615230fd7516578f3bf18bab6395e753ede7576
bictorp <victor.pintado.leiva@estudiantat.upc.edu>
2026-05-05 23:06:37 +0200

    Ejercicio Seminario - Flutter

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/user.dart';
 import '../providers/auth_provider.dart';
 import 'main_wrapper_screen.dart';
 import 'register_screen.dart';
